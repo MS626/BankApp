9fccc4081a0bc903197246fe380d3113c6529437
Miguel Santos <miguelsantos@MacBook-Air-de-Miguel-17.local>
2025-06-11 10:55:49 +0100

    Update skeleton

--- Host/src/components/Skeleton/skeletonLoader.tsx
+++ Host/src/components/Skeleton/skeletonLoader.tsx
@@ -4,8 +4,6 @@ import "../../styles/Skeleton/skeletonLoader.css";
 const SkeletonLoader: React.FC = () => {
   return (
     <div className="skeleton-loader-container">
-      <div className="skeleton-header"></div>
-      <div className="skeleton-sidebar"></div>
       <div className="skeleton-content"></div>
     </div>
   );
